7a1a2e3b469894accaf8e512edb62684053767f9
Ed Overton <e.overton@gmail.com>
2022-08-19 23:47:47 -0400

    fix via checkmks

--- metadata.rb
+++ metadata.rb
@@ -1,19 +1,12 @@
 name 'checkmka'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs/Configures checkmka'
-version '0.1.0'
-chef_version '>= 16.0'
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/checkmka/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/checkmka'
+source_url       'https://github.com/emo3/checkmka'
+issues_url       'https://github.com/emo3/checkmka/issues'
+maintainer       'Ed Overton'
+maintainer_email 'you1@example.com'
+chef_version     '>= 16.0'
+license          'Apache-2.0'
+description      'Installs/Configures checkmk agent'
+version          '1.0.0'
+supports 'redhat'
+supports 'centos'
+supports 'almalinux'
